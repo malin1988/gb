9ea1583cef61d9599135f2b1c630ccc049c7f68b
zhenpeng <zhpeng.is@gmail.com>
2011-07-15 20:47:37 +0800

    增加dister.h worker.h文件

---
+++ dister.h
@@ -0,0 +1,10 @@
+#ifdef __DISTER__H__
+#define __DISTER_H__
+
+#include "global.h"
+/**
+* 分发进程函数
+*/
+void dispatcher(raw_packet_t *rpkt);
+
+#endif
---
+++ worker.h
@@ -0,0 +1,7 @@
+#ifndef __WORKER_H__
+#define __WORKER__
+
+/**
+* 工作进程函数
+*/
+#endif
